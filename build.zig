1aaf37b692f926beb48ec720a71014386a43ae4d
OmarBouchoucha0 <bouchouchaomar38@gmail.com>
2026-06-28 18:35:10 +0100

    init raylib project

---
+++ build.zig
@@ -0,0 +1,80 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.resolveTargetQuery(.{
+        .cpu_arch = .x86_64,
+        .os_tag = .linux,
+        .abi = .gnu,
+        .glibc_version = .{ .major = 2, .minor = 36, .patch = 0 },
+    });
+    const optimize = b.standardOptimizeOption(.{});
+
+    const mod = b.addModule("astroids", .{
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+    });
+
+    const exe = b.addExecutable(.{
+        .name = "astroids",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+            .imports = &.{
+                .{ .name = "astroids", .module = mod },
+            },
+        }),
+    });
+
+    const raylib_dep = b.dependency("raylib_zig", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const raylib = raylib_dep.module("raylib"); // main raylib module
+    const raygui = raylib_dep.module("raygui"); // raygui module
+    const raylib_artifact = raylib_dep.artifact("raylib"); // raylib C library
+
+    raylib_artifact.root_module.addLibraryPath(.{ .cwd_relative = "/usr/lib" });
+    raylib_artifact.root_module.addLibraryPath(.{ .cwd_relative = "/usr/lib64" });
+    raylib_artifact.root_module.addSystemIncludePath(.{ .cwd_relative = "/usr/include" });
+
+    exe.root_module.linkLibrary(raylib_artifact);
+    exe.root_module.addImport("raylib", raylib);
+    exe.root_module.addImport("raygui", raygui);
+
+    exe.root_module.addLibraryPath(.{ .cwd_relative = "/usr/lib" });
+    exe.root_module.addLibraryPath(.{ .cwd_relative = "/usr/lib64" });
+
+    //fix linker errors
+    exe.linker_allow_shlib_undefined = true;
+
+    b.installArtifact(exe);
+
+    const run_step = b.step("run", "Run the app");
+
+    const run_cmd = b.addRunArtifact(exe);
+    run_step.dependOn(&run_cmd.step);
+
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const mod_tests = b.addTest(.{
+        .root_module = mod,
+    });
+
+    const run_mod_tests = b.addRunArtifact(mod_tests);
+
+    const exe_tests = b.addTest(.{
+        .root_module = exe.root_module,
+    });
+
+    const run_exe_tests = b.addRunArtifact(exe_tests);
+
+    const test_step = b.step("test", "Run tests");
+    test_step.dependOn(&run_mod_tests.step);
+    test_step.dependOn(&run_exe_tests.step);
+}
