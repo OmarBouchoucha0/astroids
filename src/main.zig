d165e0167651e2505f46234e6dbf96847cd01960
OmarBouchoucha0 <bouchouchaomar38@gmail.com>
2026-07-01 15:36:07 +0100

    deacceleration

--- src/main.zig
+++ src/main.zig
@@ -23,10 +23,12 @@ const SpaceShip = struct {
     PosX: i32,
     PosY: i32,
     Rot: f32,
-    Velocity: f32,
+    MaxVelocity: f32,
+    CurrentVelocity: f32,
+    Acceleration: f32,
 
     const width: i32 = 25;
-    const height: i32 = 50;
+    const height: i32 = 40;
 
     fn draw(self: SpaceShip) void {
         const w_half = @as(f32, @floatFromInt(width)) / 2.0;
@@ -81,12 +83,50 @@ const SpaceShip = struct {
             .y = self.PosY + @as(i32, @round(back_r_dy)),
         };
 
-        rl.drawLine(tip.x, tip.y, bottom_l.x, bottom_l.y, .white); // Left main side (left leg)
-        rl.drawLine(tip.x, tip.y, bottom_r.x, bottom_r.y, .white); // Right main side (right leg)
-        rl.drawLine(back_l.x, back_l.y, back_r.x, back_r.y, .white); // Inside crossbar (back of the ship)
+        rl.drawLine(tip.x, tip.y, bottom_l.x, bottom_l.y, .white);
+        rl.drawLine(tip.x, tip.y, bottom_r.x, bottom_r.y, .white);
+        rl.drawLine(back_l.x, back_l.y, back_r.x, back_r.y, .white);
     }
 
-    fn drawBoosters(self: SpaceShip) void {}
+    fn drawBoosters(self: SpaceShip) void {
+        const w_half = @as(f32, @floatFromInt(width)) / 2.0;
+        const h_half = @as(f32, @floatFromInt(height)) / 2.0;
+
+        const cos_rot = @cos(self.Rot);
+        const sin_rot = @sin(self.Rot);
+
+        const tip_dx = (0.0 * cos_rot) + (-h_half * sin_rot);
+        const tip_dy = (0.0 * sin_rot) - (-h_half * cos_rot);
+
+        const crossbar_pct = 0.85;
+        const local_back_l_x = -w_half * crossbar_pct;
+        const local_back_l_y = -h_half + (2.0 * h_half * crossbar_pct);
+        const back_l_dx = (local_back_l_x * cos_rot) - (local_back_l_y * sin_rot);
+        const back_l_dy = (local_back_l_x * sin_rot) + (local_back_l_y * cos_rot);
+
+        const local_back_r_x = w_half * crossbar_pct;
+        const local_back_r_y = local_back_l_y;
+        const back_r_dx = (local_back_r_x * cos_rot) - (local_back_r_y * sin_rot);
+        const back_r_dy = (local_back_r_x * sin_rot) + (local_back_r_y * cos_rot);
+
+        const back_l = Vec2_i32{
+            .x = self.PosX + @as(i32, @round(back_l_dx)),
+            .y = self.PosY + @as(i32, @round(back_l_dy)),
+        };
+
+        const back_r = Vec2_i32{
+            .x = self.PosX + @as(i32, @round(back_r_dx)),
+            .y = self.PosY + @as(i32, @round(back_r_dy)),
+        };
+
+        const tip = Vec2_i32{
+            .x = self.PosX + @as(i32, @round(tip_dx)),
+            .y = self.PosY + @as(i32, @round(tip_dy)),
+        };
+
+        rl.drawLine(back_l.x, back_l.y, tip.x, tip.y, .white);
+        rl.drawLine(tip.x, tip.y, back_r.x, back_r.y, .white);
+    }
 
     fn rotate(self: *SpaceShip, direction: []const u8) void {
         const rotation_speed = 5.0;
@@ -103,10 +143,34 @@ const SpaceShip = struct {
     fn fly(self: *SpaceShip) void {
         const sin_rot = @sin(self.Rot);
         const cos_rot = @cos(self.Rot);
-        const dx = sin_rot * self.Velocity;
-        const dy = cos_rot * self.Velocity;
+
+        self.CurrentVelocity *= self.Acceleration;
+
+        if (self.CurrentVelocity < 1) self.CurrentVelocity = 1;
+        if (self.CurrentVelocity > self.MaxVelocity) self.CurrentVelocity = self.MaxVelocity;
+
+        const dx = sin_rot * self.CurrentVelocity;
+        const dy = cos_rot * self.CurrentVelocity;
+
         self.PosX += @as(i32, @round(dx));
         self.PosY -= @as(i32, @round(dy));
+    }
+
+    fn deAccelerate(self: *SpaceShip) void {
+        const sin_rot = @sin(self.Rot);
+        const cos_rot = @cos(self.Rot);
+
+        if (self.CurrentVelocity > 0.0) {
+            self.CurrentVelocity /= self.Acceleration;
+
+            if (self.CurrentVelocity < 0.1) self.CurrentVelocity = 0.0;
+
+            const dx = sin_rot * self.CurrentVelocity;
+            const dy = cos_rot * self.CurrentVelocity;
+
+            self.PosX += @as(i32, @round(dx));
+            self.PosY -= @as(i32, @round(dy));
+        }
     }
 };
 
@@ -115,8 +179,8 @@ pub fn main() !void {
     defer arena.deinit();
     const allocator = arena.allocator();
 
-    const screenWidth = 1600;
-    const screenHeight = 900;
+    const screenWidth = 900;
+    const screenHeight = 600;
 
     rl.setConfigFlags(.{ .msaa_4x_hint = true });
     rl.initWindow(screenWidth, screenHeight, "Astroids");
@@ -130,7 +194,9 @@ pub fn main() !void {
         .PosX = screenWidth / 2,
         .PosY = screenHeight / 2,
         .Rot = 0,
-        .Velocity = 5,
+        .MaxVelocity = 5,
+        .CurrentVelocity = 0,
+        .Acceleration = 1.05,
     };
 
     while (!rl.windowShouldClose()) {
@@ -149,10 +215,15 @@ pub fn main() !void {
         }
         if (rl.isKeyDown(.up)) {
             spaceship.fly();
+            spaceship.drawBoosters();
+        } else {
+            spaceship.deAccelerate();
         }
         spaceship.draw();
 
         info = try std.fmt.allocPrintSentinel(allocator, "fps : {d:0>3} screen : {d}x{d}", .{ fps, screenWidth, screenHeight }, 0);
         rl.drawText(info, 5, 5, 10, .red);
+        info = try std.fmt.allocPrintSentinel(allocator, "Velocity : {} rot : {}", .{ spaceship.CurrentVelocity, spaceship.Rot }, 0);
+        rl.drawText(info, 5, 20, 10, .red);
     }
 }
